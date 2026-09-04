b61f66c38fd16134679ca64c3d1df4fd206c9a70
Shivam Murawala <Shivammurawala2810@gmail.com>
2026-09-04 17:00:23 -0400

    Improved UI styling and added contacts feature

--- src/ComputerScreen/ComputerScreen.jsx
+++ src/ComputerScreen/ComputerScreen.jsx
@@ -1,9 +1,14 @@
 import './ComputerScreen.css'
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import monitorBorder from '../assets/monitorbackground.png'
 
-
 function ComputerScreen() {
+
+  //Allows access to the input element
+  const inputRef = useRef(null);
+
+  //Allows access to terminal div element
+  const terminalRef = useRef()
 
   //Tracks history of console
   const [history, setHistory] = useState([{ type: 'output', content: "Welcome to shivam_os v1.0. Type 'help' to see available commands." }]);
@@ -14,8 +19,54 @@ function ComputerScreen() {
   //Variable for final returned history
   let newHistory = [];
 
+  //Function to process commands and  return string
+  function processCommand(input) {
+
+    switch (input) {
+      case 'help':
+        return <div style={{ whiteSpace: "pre-wrap" }}>
+          AVAILABLE COMMANDS:<br></br>
+          help      -Display this message<br></br>
+          whoami    -Brief Introduction and bio<br></br>
+          resume    -Download my resume<br></br>
+          contact   -Get Contact Info <br></br>
+          clear     -Clear Screen
+        </div>
+
+      case 'whoami':
+        return <div>
+          Name: Shivam Murawala<br></br>
+          Status: High School Senior at Woburn Collegiate Institute<br></br>
+          Interests: Math Problemsolving, Business/DECA, Programming, Art, NBA/Basketball, Distance Running<br></br>
+
+        </div>
+
+      case 'resume':
+
+        return <div>
+          Fetching file: Shivam_Murawala_Highschool_Dev.pdf... <br></br>
+          Initiating Download sequence... <br></br>
+          <a rel="noopener noreferrer" target="_blank" href="./resume.pdf" style={{ color: "rgb(255, 255, 177)" }}>Click to Download</a>
+        </div>
+
+      case 'contact':
+        return <div style={{ whiteSpace: "pre-wrap" }}>
+          <br></br>
+          {">"} Initiating handshake configurations... <br></br>
+          Email:    <a style={{ color: "inherit" }} target="_blank" href='mailto:shivammurawala2810@gmail.com'>shivammurawala2810@gmail.com</a><br></br>
+          Github:   <a style={{ color: "inherit" }} target="_blank" href='https://github.com/broken-proof'>github.com/broken-proof</a><br></br>
+          LinkedIn: <a style={{ color: "inherit" }} target="_blank" href='https://www.linkedin.com/in/shivam-murawala-b9141829b/'>linkedin.com/in/shivam-murawala-b9141829b/</a>
+        </div>
+
+      default:
+        return <div>
+          Command Not Found {":("}
+        </div>
+    }
+  }
+
   //Function to handle user inputs for input field
-  const handleInput = (element) => {
+  function handleInput(element) {
     if (element.key === "Enter") {
       const fixedInput = input.trim().toLowerCase();
 
@@ -53,42 +104,34 @@ function ComputerScreen() {
       setInput("");
     }
 
-    //Function to process commands and  return string
-    function processCommand(input) {
-
-      switch (input) {
-        case 'help':
-          return <div style={{ whiteSpace: "pre-wrap" }}>
-            AVAILABLE COMMANDS:<br></br>
-            help      -Display this message<br></br>
-            whoami    -Brief Introduction and bio<br></br>
-            resume    -Download my resume<br></br>
-            clear     -Clear Screen
-          </div>
-
-        case 'whoami':
-          return <div>
-            Name: Shivam Murawala<br></br>
-            Status: High School Senior at Woburn Collegiate Institute<br></br>
-            Interests: Math Problemsolving, Business/DECA, Programming, Art, NBA/Basketball, Distance Running<br></br>
-
-          </div>
-
-        case 'resume':
-
-          return <div>
-            Fetching file: Shivam_Murawala_Highschool_Dev.pdf... <br></br>
-            Initiating Download sequence... <br></br>
-            <a rel="noopener noreferrer" target="_blank" href="./resume.pdf" style={{ color: "rgb(255, 255, 177)" }}>Click to Download</a>
-          </div>
-
-        default:
-          return <div>
-            Command Not Found {":("}
-          </div>
-      }
-    }
   }
+
+  //Function to focus back on input when any part of the terminal is clicked
+  function focusOnInput() {
+    inputRef.current.focus();
+  }
+
+  //Helper function to give a delay before scrolling down
+  function timer() {
+    let scroller = setTimeout(() => {
+      terminalRef.current.scrollTo({
+        top: terminalRef.current.scrollHeight,
+        behavior: 'smooth'
+      });
+    }, 50)
+    return scroller;
+  }
+
+  //Scroll back into view whenever history changes
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      terminalRef.current?.scrollTo({
+        top: terminalRef.current.scrollHeight,
+        behavior: 'smooth'
+      });
+    })
+  }, [history])
+
   return (
 
     <div className="monitor">
@@ -96,7 +139,7 @@ function ComputerScreen() {
 
         <div className="crt">
 
-          <div className="terminal">
+          <div ref={terminalRef} className="terminal" onClick={focusOnInput}>
 
             {/* Render all lines within the history */}
             {
@@ -119,9 +162,8 @@ function ComputerScreen() {
             {/* Render Active Input  */}
             <div style={{ display: "flex", alignItems: "center" }}>
               <span className='output_line'>guest@shivam-os:$ </span>
-              <input onKeyDown={handleInput} className="current_input" type="text" value={input} autoFocus spellCheck="false" autoComplete="off" onChange={(element) => setInput(element.target.value)}></input>
+              <input ref={inputRef} onKeyDown={handleInput} className="current_input" type="text" value={input} autoFocus spellCheck="false" autoComplete="off" onChange={(element) => setInput(element.target.value)}></input>
             </div>
-
           </div>
 
         </div>
